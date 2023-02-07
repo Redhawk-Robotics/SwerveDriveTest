f0789891cf8b670613400250b0868212a0b59384
Giankyle Vallarta <kyle101206@users.noreply.github.com>
2023-02-07 11:51:15 -0500

    First Init

---
+++ src/main/java/frc/robot/constants/Ports.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+/**
+ * @Filename:Ports.java
+ * @Purpose:This file contains the different ports of motors, and sensors
+ * @Version:1.0
+ * @Author: TEAM 8739 RedhawkRobotics
+ * @Date: 02/07/23
+ */
+
+package frc.robot.constants;
+
+public interface Ports {
+    public interface Gamepad{
+        int DRIVER = 0;
+        int OPERATOR = 1;
+        int DEBUGGER = 2;
+    }
+}
