8f650b90e3b7c1e4c1ddd613f2085fe423464a68
Giankyle Vallarta <kyle101206@users.noreply.github.com>
2023-02-25 12:24:51 -0500

    lol

--- src/main/java/frc/robot/Autons/AutoBalance.java
+++ src/main/java/frc/robot/Autons/AutoBalance.java
@@ -4,18 +4,31 @@
 
 package frc.robot.Autons;
 
+import com.ctre.phoenix.sensors.Pigeon2;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.constants.Ports;
+import frc.robot.subsystems.SwerveSubsystem;
 
 public class AutoBalance extends CommandBase {
   /** Creates a new AutoBalance. */
+  
+  private Pigeon2 m_Pigeon = new Pigeon2(Ports.Gyro.DRIVETRAIN_PIGEON_ID);
+  private SwerveSubsystem s_Swerve;
+
 
   public AutoBalance() {
     // Use addRequirements() here to declare subsystem dependencies.
-  }
+  addRequirements(s_Swerve);  
+}
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+
+    SmartDashboard.getNumber("Yaw",m_Pigeon.getYaw());
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
