870965020f3555565e06bc9acacf0e9bc8815d80
Michael Carcamo <michaelc978@nycstudents.net>
2023-02-25 13:30:18 -0500

    create Gyro subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import frc.robot.commands.Swerve.Drive;
 import frc.robot.constants.Ports;
+import frc.robot.subsystems.GyroSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
@@ -17,120 +18,124 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
 
   // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
- 
-/* Subsystems */
-private final SwerveSubsystem SwerveDrive = new SwerveSubsystem();
 
-/* Commands */
+  /* Subsystems */
+  private final SwerveSubsystem SwerveDrive = new SwerveSubsystem();
+  private final GyroSubsystem gyro = new GyroSubsystem();
 
-// Replace with CommandPS4Controller or CommandJoystick if needed
-/* Controllers */
-private final XboxController DRIVER =
-    new XboxController(Ports.Gamepad.DRIVER);
+  /* Commands */
 
-private final XboxController OPERATOR =
-    new XboxController(Ports.Gamepad.OPERATOR);
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  /* Controllers */
+  private final XboxController DRIVER = new XboxController(Ports.Gamepad.DRIVER);
 
-/* Drive Controls */
-private final int translationAxis = XboxController.Axis.kLeftY.value;
-private final int strafeAxis = XboxController.Axis.kLeftX.value;
-private final int rotationAxis = XboxController.Axis.kRightX.value;
+  private final XboxController OPERATOR = new XboxController(Ports.Gamepad.OPERATOR);
 
-/* Driver Buttons */
-private final Trigger zeroGyro =
-    new JoystickButton(DRIVER, XboxController.Button.kA.value);
-private final Trigger robotCentric =
-    new JoystickButton(DRIVER, XboxController.Button.kY.value);
+  /* Drive Controls */
+  private final int translationAxis = XboxController.Axis.kLeftY.value;
+  private final int strafeAxis = XboxController.Axis.kLeftX.value;
+  private final int rotationAxis = XboxController.Axis.kRightX.value;
 
-private final Trigger slowSpeed =
-    new JoystickButton(DRIVER, XboxController.Button.kRightBumper.value);
+  /* Driver Buttons */
+  private final Trigger zeroGyro = new JoystickButton(DRIVER, XboxController.Button.kA.value);
+  private final Trigger robotCentric = new JoystickButton(DRIVER, XboxController.Button.kY.value);
 
-//Additional buttons
-private final Trigger highGrid = 
-    new JoystickButton(DRIVER, XboxController.Button.kLeftBumper.value);
+  private final Trigger slowSpeed = new JoystickButton(DRIVER, XboxController.Button.kRightBumper.value);
 
-private final Trigger Tester = 
-    new JoystickButton(DRIVER, XboxController.Button.kB.value);
+  // Additional buttons
+  private final Trigger highGrid = new JoystickButton(DRIVER, XboxController.Button.kLeftBumper.value);
 
-  
-    
-//TODO May need to switch the object for each button to JoystickButton
-// Create SmartDashboard chooser for autonomous routines
-private static SendableChooser<Command> Autons = new SendableChooser<>();
+  private final Trigger Tester = new JoystickButton(DRIVER, XboxController.Button.kB.value);
 
-/** The container for the robot. Contains subsystems, OI devices, and commands. */
-public RobotContainer() {
-SwerveDrive.setDefaultCommand(
-  new Drive(
-    SwerveDrive,
-      () -> -DRIVER.getRawAxis(translationAxis),
-      () -> -DRIVER.getRawAxis(strafeAxis),
-      () -> -DRIVER.getRawAxis(rotationAxis),
-      () -> robotCentric.getAsBoolean(),
-      () -> slowSpeed.getAsBoolean()));
-
-// Configure the trigger bindings, defaults, Autons
-  configureDefaultCommands();
-  configureButtonBindings();
-  configureAutons();
-}
+  // TODO May need to switch the object for each button to JoystickButton
+  // Create SmartDashboard chooser for autonomous routines
+  private static SendableChooser<Command> Autons = new SendableChooser<>();
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    SwerveDrive.setDefaultCommand(
+        new Drive(
+            SwerveDrive,
+            () -> -DRIVER.getRawAxis(translationAxis),
+            () -> -DRIVER.getRawAxis(strafeAxis),
+            () -> -DRIVER.getRawAxis(rotationAxis),
+            () -> robotCentric.getAsBoolean(),
+            () -> slowSpeed.getAsBoolean()));
+
+    // Configure the trigger bindings, defaults, Autons
+    configureDefaultCommands();
+    configureButtonBindings();
+    configureAutons();
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
 
-/****************/
-/*** DEFAULTS ***/
-/****************/
+  /****************/
+  /*** DEFAULTS ***/
+  /****************/
 
-private void configureDefaultCommands() {}
+  private void configureDefaultCommands() {
+  }
 
-/***************/
-/*** BUTTONS ***/
-/***************/
+  /***************/
+  /*** BUTTONS ***/
+  /***************/
 
   private void configureButtonBindings() {
-  // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-  // new Trigger(m_exampleSubsystem::exampleCondition)
-  //     .onTrue(new ExampleCommand(m_exampleSubsystem));
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    // new Trigger(m_exampleSubsystem::exampleCondition)
+    // .onTrue(new ExampleCommand(m_exampleSubsystem));
 
-  // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-  // cancelling on release.
-  
-  //DRVIER.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-  zeroGyro.onTrue(new InstantCommand(() -> SwerveDrive.zeroGyro()));//A value for the Xbox Controller
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is
+    // pressed,
+    // cancelling on release.
 
-  //System.out.print("Swervy");
-  
-  // slowSpeed.onTrue(new InstantCommand(() -> SwerveDrive.slowSpeed()));
+    // DRVIER.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    zeroGyro.onTrue(new InstantCommand(() -> SwerveDrive.zeroGyro()));// A value for the Xbox Controller
 
-  // robotCentric.onTrue(new InstantCommand(() -> SwerveDrive.robotCentric()));
+    // System.out.print("Swervy");
+
+    // slowSpeed.onTrue(new InstantCommand(() -> SwerveDrive.slowSpeed()));
+
+    // robotCentric.onTrue(new InstantCommand(() -> SwerveDrive.robotCentric()));
 
   }
-  
+
   /**************/
   /*** AUTONS ***/
   /**************/
 
   public void configureAutons() {
-    //autonChooser.setDefaultOption("Do Nothing", new DoNothingAuton());
+    // autonChooser.setDefaultOption("Do Nothing", new DoNothingAuton());
     SmartDashboard.putData("Autonomous: ", Autons);
-}
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
--- src/main/java/frc/robot/commands/Autons/AutoBalance.java
+++ src/main/java/frc/robot/commands/Autons/AutoBalance.java
@@ -2,37 +2,43 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.Autons;
+package frc.robot.commands.Autons;
 
 import com.ctre.phoenix.sensors.Pigeon2;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.constants.Ports;
+import frc.robot.subsystems.GyroSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.subsystems.modules.PiegeonModule;
 
 public class AutoBalance extends CommandBase {
   /** Creates a new AutoBalance. */
   
-  private Pigeon2 m_Pigeon = new Pigeon2(Ports.Gyro.DRIVETRAIN_PIGEON_ID);
+  private GyroSubsystem m_Gyro; 
   private SwerveSubsystem s_Swerve;
 
 
-  public AutoBalance() {
+  public AutoBalance(SwerveSubsystem s_Swerve, GyroSubsystem m_Gyro) {
     // Use addRequirements() here to declare subsystem dependencies.
-  addRequirements(s_Swerve);  
+    this.s_Swerve = s_Swerve;
+    this.m_Gyro = m_Gyro;
+    addRequirements(s_Swerve, m_Gyro);
 }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
-    SmartDashboard.getNumber("Yaw",m_Pigeon.getYaw());
+    this.m_Gyro.setYaw(0);
+    SmartDashboard.getNumber("Yaw", this.m_Gyro.getYaw());
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
