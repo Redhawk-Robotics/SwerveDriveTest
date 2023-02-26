81aea4fc117a2ed26e839d4528f01b200c876e19
Giankyle Vallarta <kyle101206@users.noreply.github.com>
2023-02-26 14:21:02 -0500

    Fix the commands for each subsystem

--- src/main/java/frc/robot/commands/test/ArmTesterCommand.java
+++ src/main/java/frc/robot/commands/test/ArmTesterCommand.java
@@ -2,13 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.Claw;
+package frc.robot.commands.test;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class Claw extends CommandBase {
-  /** Creates a new Claw. */
-  public Claw() {
+public class ArmTesterCommand extends CommandBase {
+  /** Creates a new Arm. */
+  public ArmTesterCommand() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
--- src/main/java/frc/robot/commands/test/ClawTesterCommand.java
+++ src/main/java/frc/robot/commands/test/ClawTesterCommand.java
@@ -2,14 +2,22 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.Claw;
+package frc.robot.commands.test;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.modules.CompressorModule;
+import frc.robot.test.clawTest;
 
-public class Claw extends CommandBase {
+public class ClawTesterCommand extends CommandBase {
   /** Creates a new Claw. */
-  public Claw() {
+  private clawTest claw;
+  private CompressorModule compressor;
+  
+  public ClawTesterCommand(clawTest claw) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.claw = claw;
+    this.compressor = CompressorModule.getCompressorModule();
+    addRequirements(claw);
   }
 
   // Called when the command is initially scheduled.
@@ -18,7 +26,13 @@ public class Claw extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    this.compressor.enableAnalog();
+    this.claw.stopMotors();
+  }
+
+  // ADD TRIGGERS FOR BUTTON
+  // WHEN PRESSED OPEN CLAW AND MOVE THE MOTORS
 
   // Called once the command ends or is interrupted.
   @Override
