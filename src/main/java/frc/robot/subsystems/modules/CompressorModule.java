ec042f2d50b60287f02db902978c89df96614bad
Michael Carcamo <michaelc978@nycstudents.net>
2023-03-14 22:22:11 -0400

    added the goofy PneumaticHub class

--- src/main/java/frc/robot/subsystems/modules/CompressorModule.java
+++ src/main/java/frc/robot/subsystems/modules/CompressorModule.java
@@ -4,15 +4,18 @@ import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import frc.robot.constants.Setting;
 import edu.wpi.first.wpilibj.PneumaticHub;
+import edu.wpi.first.wpilibj.PneumaticsBase;
 
 public class CompressorModule {
 
-    private final Compressor compressor;
-    
+    public Compressor compressor;
+    public PneumaticHub pneumaticHub;
     private static CompressorModule singleton;
 
     public CompressorModule() {
-        this.compressor = new Compressor(PneumaticsModuleType.REVPH);
+        singleton.pneumaticHub = new PneumaticHub();
+        // Why make this then???
+        singleton.compressor = ((PneumaticsBase) compressor).makeCompressor();
     }
 
     public static CompressorModule getCompressorModule() {
@@ -22,25 +25,28 @@ public class CompressorModule {
         return singleton;
     }
 
-    public void enableAnalog(double min, double max) {
-        compressor.enableAnalog(min, max);
+    public void enableCompressorAnalog(double min, double max) {
+        singleton.pneumaticHub.enableCompressorAnalog(min, max);
     }
 
+    public void enableCompressorDigital() {
+        singleton.pneumaticHub.enableCompressorDigital();
+    }
     public boolean isEnabled() {
-        return compressor.isEnabled();
+        return singleton.pneumaticHub.getCompressor();
     }
 
     public double getPressure() {
-        return compressor.getPressure();
+        return singleton.compressor.getPressure();
     }
 
-    public void setPressureAnalog(double pressure) {
-        if (pressure == 220) {
-            compressor.disable();
-        } else if (pressure >= 60) {
-            enableAnalog(Setting.compressor.absoluteMinPressure, Setting.compressor.absoluteMaxPressure);
-        } else {
-            enableAnalog(Setting.compressor.relativeMinPressure, Setting.compressor.absoluteMaxPressure);
-        }
-    }
+    // public void setPressureAnalog(double pressure) {
+    //     if (pressure == 220) {
+    //         compressor.disable();
+    //     } else if (pressure >= 60) {
+    //         enableAnalog(Setting.compressor.absoluteMinPressure, Setting.compressor.absoluteMaxPressure);
+    //     } else {
+    //         enableAnalog(Setting.compressor.relativeMinPressure, Setting.compressor.absoluteMaxPressure);
+    //     }
+    // }
 }
